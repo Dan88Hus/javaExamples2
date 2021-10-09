f074bedb8a75d3f0ada727751e2aada3917afb6e
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2021-10-09 15:42:02 +0300

    master p2

--- OOPMaster/src/Main.java
+++ OOPMaster/src/Main.java
@@ -8,5 +8,12 @@ public class Main {
         price = hamburger.itemizeHamburger();
         System.out.println("Total price is: "+ price);
 
+        HealtyBurger healtyBurger = new HealtyBurger("No meat", 3.50);
+        healtyBurger.addHealthyAddition1("tomato",0.50);
+//        hamburger.addHamburgerAddition2("Lettuce",0.50);
+//        hamburger.addHamburgerAddition3("Cheese",0.50);
+        price = healtyBurger.itemizeHamburger();
+        System.out.println("Total price is: "+ price);
+
     }
 }
