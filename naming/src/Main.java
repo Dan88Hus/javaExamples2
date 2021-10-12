3ae9747c2862603019e59c0a2233024a12df7226
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2021-10-12 10:14:55 +0300

    naming

---
+++ naming/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+//        packages
+//        always lower case, unique name, com.abcxyz
+//        class names
+//        camel case, class names are nouns(they represent things),start with capital letter, eg:LinkedList
+//        interface camelcase as well
+//        methods : getName(), addPlayer(), etc ...
+//        constants: static final int MAX_INT
+
+
+    }
+
+}
